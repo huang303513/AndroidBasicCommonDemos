d66a49e7709ea1ea7b5421fe7838e4dadc5a083a
huangchengdu <394042583@qq.com>
2016-08-22 21:01:27 +0800

    post方式

--- Basic/android4POST网络请求/app/src/main/java/com/ctrip/www/android4post/HttpUtils.java
+++ Basic/android4POST网络请求/app/src/main/java/com/ctrip/www/android4post/HttpUtils.java
@@ -1,5 +1,8 @@
 package com.ctrip.www.android4post;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -51,14 +54,42 @@ public class HttpUtils {
             //设置请求体的一些属性,表示设置请求体的类型是文本类型
             urlConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
             urlConnection.setRequestProperty("Content-Length",String.valueOf(mydata.length));
-            //获取输出流/向服务器输出数据
+            //获取输出流,向服务器输出数据
             OutputStream outputStream = urlConnection.getOutputStream();
             outputStream.write(mydata);
-            
+            //获得服务器响应的结果和状态码
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == 200){
+                return changeInputStream(urlConnection.getInputStream(),encode);
+            }
             //System.out.println("TEST" + buffer.toString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return  buffer.toString();
+        return  "";
+    }
+
+    /**
+     * 将一个输入流转换成字符串
+     * @param inputStream
+     * @param encode
+     * @return
+     */
+    private static String changeInputStream(InputStream inputStream, String encode){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int len = 0;
+        String result = "";
+        if (inputStream != null){
+            try {
+                while ((len = inputStream.read(data)) != -1) {
+                    outputStream.write(data, 0, len);
+                }
+                result = new String(outputStream.toByteArray(),encode);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  null;
     }
 }

1f4ec35538082813016bf549cb6ca4b423e44607
huangchengdu <394042583@qq.com>
2016-08-30 18:19:31 +0800

    Service的使用与绑定

---
+++ Basic/绑定本地Service并与之通信/app/src/main/java/com/ctrip/www/ServiceUtils/BindService.java
@@ -0,0 +1,69 @@
+package com.ctrip.www.ServiceUtils;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * 项目名称：绑定本地Service并与之通信
+ * 类描述：
+ * 创建人：huangchengdu
+ * 创建时间：16/8/30 下午5:45
+ * 修改人：huangchengdu
+ * 修改时间：16/8/30 下午5:45
+ * 修改备注：
+ */
+public class BindService extends Service{
+    private int count;
+    private boolean quit;
+    private MyBinder binder = new MyBinder();
+
+    public class MyBinder extends Binder{
+        public int getCount(){
+            return count;
+        }
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        System.out.println("Service is Binded");
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("Service is Created");
+        //启动一条线程/动态地修改count的值
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                while (!quit){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    count++;
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        System.out.println("Service is unBinded");
+        return super.onUnbind(intent);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.quit = true;
+        System.out.println("Service is Destroyed");
+    }
+}
---
+++ Basic/绑定本地Service并与之通信/app/src/main/java/com/ctrip/www/service/MainActivity.java
@@ -0,0 +1,64 @@
+package com.ctrip.www.service;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ctrip.www.ServiceUtils.BindService;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bind,unbind,getServiceStatus;
+    private BindService.MyBinder binder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            System.out.println("--Service Connected--");
+            binder = (BindService.MyBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            System.out.println("--Service Disconnected--");
+        }
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bind = (Button) findViewById(R.id.bind);
+        unbind = (Button) findViewById(R.id.unbind);
+        getServiceStatus = (Button) findViewById(R.id.getServiceStatus);
+
+        //创建启动Service的Intent
+        final Intent intent = new Intent(this,BindService.class);
+
+        bind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bindService(intent,connection, Service.BIND_AUTO_CREATE);
+            }
+        });
+
+        unbind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unbindService(connection);
+            }
+        });
+
+        getServiceStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Service 的count值是:" + binder.getCount(),Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+    }
+}

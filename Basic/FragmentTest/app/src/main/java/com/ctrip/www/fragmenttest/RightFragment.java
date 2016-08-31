cdb5d3aa6838eef959474026e1d72d650d48e397
huangchengdu <394042583@qq.com>
2016-08-31 18:56:06 +0800

    Fragment的基本使用

---
+++ Basic/FragmentTest/app/src/main/java/com/ctrip/www/fragmenttest/RightFragment.java
@@ -0,0 +1,28 @@
+package com.ctrip.www.fragmenttest;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RightFragment extends Fragment {
+
+
+    public RightFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment,container,false);
+        return view;
+    }
+
+}

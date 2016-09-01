20f2657ad88d91b6b3d1af3e11d076eead8eb7b3
黄成都 <394042583@qq.com>
2016-09-02 07:51:25 +0800

    显示省市县

--- 酷欧天气/app/src/main/java/com/ctrip/www/activity/ChooseAreaActivity.java
+++ 酷欧天气/app/src/main/java/com/ctrip/www/activity/ChooseAreaActivity.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -70,10 +72,27 @@ public class ChooseAreaActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         //初始化实例
         coolWeatherDB = CoolWeatherDB.getInstance(this);
+        //添加点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (currentLevel == LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(i);
+                    queryCities();
+                }else if (currentLevel == LEVEL_CITY){
+                    selectedCity = cityList.get(i);
+                    queryCounties();
+                }
+            }
+        });
+
         //加载省份数据
         queryProvinces();
     }
 
+    /**
+     * 加载省份信息
+     */
     private void queryProvinces() {
         //从数据库加载数据
         provinceList = coolWeatherDB.loadProvinces();
@@ -91,6 +110,49 @@ public class ChooseAreaActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 查询选中省内所有的市,优先从数据库查询、如果没有查询到再去服务器查询
+     */
+    private void queryCities(){
+        cityList = coolWeatherDB.loadCities(selectedProvince.getId());
+        if (cityList.size() > 0){
+            dataList.clear();
+            for (City city:cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            titleText.setText(selectedProvince.getProvinceName());
+            currentLevel = LEVEL_CITY;
+        }else {
+            queryFromServer(selectedProvince.getProvinceCode(),"city");
+        }
+    }
+
+    /**
+     * 查询选中市内的所有县,优先数据库查询,如果么与查询到再去服务器查询
+     */
+    private void  queryCounties(){
+        countyList = coolWeatherDB.loadCounties(selectedCity.getId());
+        if (countyList.size() > 0) {
+            dataList.clear();
+            for (County county : countyList) {
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            titleText.setText(selectedCity.getCityName());
+            currentLevel = LEVEL_COUNTY;
+        } else {
+            queryFromServer(selectedCity.getCityCode(), "county");
+        }
+    }
+
+    /**
+     * 根据传入的代号和类型从服务器上查询省市县数据
+     * @param code
+     * @param type
+     */
     private void queryFromServer(final String code, final String type) {
         String address;
         if (!TextUtils.isEmpty(code)) {
@@ -99,7 +161,7 @@ public class ChooseAreaActivity extends AppCompatActivity {
         } else {
             address = "http://www.weather.com.cn/data/list3/city.xml";
         }
-        //showProgressDialog();
+        showProgressDialog();
         HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
             @Override
             public void onFinish(String response) {
@@ -118,13 +180,13 @@ public class ChooseAreaActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            //closeProgressDialog();
+                            closeProgressDialog();
                             if ("province".equals(type)) {
                                 queryProvinces();
                             } else if ("city".equals(type)) {
-                                //queryCities();
+                                queryCities();
                             } else if ("county".equals(type)) {
-                                //queryCounties();
+                                queryCounties();
                             }
                         }
                     });
@@ -141,5 +203,38 @@ public class ChooseAreaActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    /**
+     * 显示进度对话框
+     */
+    private void showProgressDialog(){
+        if (progressDialog == null){
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("正在加载...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog(){
+        if (progressDialog != null){
+            progressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if (currentLevel == LEVEL_COUNTY){
+            queryCities();
+        }else if (currentLevel == LEVEL_CITY){
+            queryProvinces();
+        }else {
+            finish();
+        }
     }
 }

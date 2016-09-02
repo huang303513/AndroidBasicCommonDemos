fc680937cb28ccdd438bcc51af1ee3356258e0df
黄成都 <394042583@qq.com>
2016-09-02 08:45:43 +0800

    天气

---
+++ 酷欧天气/app/src/main/java/com/ctrip/www/activity/WeatherActivity.java
@@ -0,0 +1,112 @@
+package com.ctrip.www.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ctrip.www.android.R;
+
+public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout weatherInfoLayout;
+    /**
+     * 用于显示城市名
+     */
+    private TextView cityNameText;
+    /**
+     * 用于显示发布时间
+     */
+    private TextView publishText;
+    /**
+     * 用于显示天气描述信息
+     */
+    private TextView weatherDespText;
+    /**
+     * 用于显示气温1
+     */
+    private TextView temp1Text;
+    /**
+     * 用于显示气温2
+     */
+    private TextView temp2Text;
+    /**
+     * 用于显示当前日期
+     */
+    private TextView currentDateText;
+    /**
+     * 切换城市按钮
+     */
+    private Button switchCity;
+    /**
+     * 更新天气按钮
+     */
+    private Button refreshWeather;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        // 初始化各控件
+        weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView) findViewById(R.id.city_name);
+        publishText = (TextView) findViewById(R.id.publish_text);
+        weatherDespText = (TextView) findViewById(R.id.weather_desp);
+        temp1Text = (TextView) findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDateText = (TextView) findViewById(R.id.current_date);
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+
+        String countyCode = getIntent().getStringExtra("county_code");
+        if (!TextUtils.isEmpty(countyCode)){
+            //有县级代号就去查询天气
+            publishText.setText("同步中");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            //queryWeatherCode(countyCode);
+        }else {
+            // 没有县级代号时就直接显示本地天气
+            showWeather();
+        }
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.switch_city:
+                Intent intent = new Intent(this,ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity",true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("同步中...");
+
+        }
+    }
+
+    /**
+     * 从SharedPreferences文件中读取存储的天气信息,并显示到界面上。 */
+    private void showWeather() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText( prefs.getString("city_name", ""));
+        temp1Text.setText(prefs.getString("temp1", ""));
+        temp2Text.setText(prefs.getString("temp2", ""));
+        weatherDespText.setText(prefs.getString("weather_desp", ""));
+        publishText.setText("今天" + prefs.getString("publish_time", "") + "发布");
+        currentDateText.setText(prefs.getString("current_date", ""));
+        weatherInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+    }
+}
--- 酷欧天气/app/src/main/java/com/ctrip/www/util/Utility.java
+++ 酷欧天气/app/src/main/java/com/ctrip/www/util/Utility.java
@@ -10,12 +10,23 @@ package com.ctrip.www.util;
  * 修改备注：
  */
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.widget.ShareActionProvider;
 
 import com.ctrip.www.db.CoolWeatherDB;
 import com.ctrip.www.model.City;
 import com.ctrip.www.model.County;
 import com.ctrip.www.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * 处理服务器返回的数据
@@ -87,5 +98,46 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 解析服务器返回的JSON数据,并将解析出的数据存储到本地
+     * @param context
+     * @param response
+     */
+    public static void handleWeatherResponse(Context context,String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中
+     */
+    public static void saveWeatherInfo(Context context,String cityName, String weatherCode,
+                                       String temp1, String temp2, String weatherDesp, String
+                                               publishTime){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context)
+                .edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
+        editor.commit();
+
     }
 }

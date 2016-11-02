30c2f2f17a446f7b545c203a75e3fcf60c9f8a53
huangchengdu <394042583@qq.com>
2016-11-02 22:23:20 +0800

    ArrayAdapter的使用

--- ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
+++ ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
@@ -2,12 +2,25 @@ package www.ctrip.com.listviewarrayadaptersimpleadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView mListView;
+    private ArrayAdapter<String> mArrayAdapter;
+    private SimpleAdapter mSimpleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.listView);
+        //新建一个数据适配器
+        //参数说明：ArrayAdapter(上下文，当前listView加载的每个列表项对应的布局文件，数据源);
+        //适配器加载数据源
+        String[]arrayData = {"慕课网1","慕课网2","慕课网3","慕课网4","慕课网5","慕课网6"};
+        mArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayData);
+        //视图(ListView)加载适配器
+        mListView.setAdapter(mArrayAdapter);
     }
 }

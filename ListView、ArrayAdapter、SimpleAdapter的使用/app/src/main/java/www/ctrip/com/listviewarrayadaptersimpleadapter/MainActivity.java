1598353ce490b4f40c6516d614460b52626f3e58
huangchengdu <394042583@qq.com>
2016-11-02 23:06:39 +0800

    SimpleAdapter的使用

--- ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
+++ ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
@@ -1,14 +1,20 @@
 package www.ctrip.com.listviewarrayadaptersimpleadapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
     private ArrayAdapter<String> mArrayAdapter;
+    private List<Map<String,Object>> dataList;
     private SimpleAdapter mSimpleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
         String[]arrayData = {"慕课网1","慕课网2","慕课网3","慕课网4","慕课网5","慕课网6"};
         mArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayData);
         //视图(ListView)加载适配器
-        mListView.setAdapter(mArrayAdapter);
+       // mListView.setAdapter(mArrayAdapter);
+
+
+        //SimpAdapter适配器参数说明
+        //context：上下文
+        //data：数据源(List<? extends Map<String,?>> data)一个Map所组成的List集合。每个Map都会对应ListView列表中的一行，每个Map(健值对)
+        // 中的健必须包含所有在from中所指的健。
+        //resource:列表项中布局文件的ID
+        //from:Map中的健名
+        //to:绑定数据是同中的ID，与from成对应关系
+        dataList = new ArrayList<Map<String,Object>>();
+        mSimpleAdapter = new SimpleAdapter(this,getData(), R.layout.item,new String[]{"pic","text"},new int[]{
+            R.id.pic, R.id.text});
+        mListView.setAdapter(mSimpleAdapter);
+
+
+    }
+
+    private List<Map<String,Object>> getData(){
+        for (int i=0; i< 20; i++){
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put("pic", R.mipmap.ic_launcher);
+            map.put("text","慕课网" + i);
+            dataList.add(map);
+        }
+        return dataList;
     }
 }

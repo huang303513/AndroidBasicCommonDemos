12a106dcfce31a25730c3114c905fe8841b81691
huangchengdu <394042583@qq.com>
2016-11-03 08:00:23 +0800

    ListView点击事件添加

--- ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
+++ ListView、ArrayAdapter、SimpleAdapter的使用/app/src/main/java/www/ctrip/com/listviewarrayadaptersimpleadapter/MainActivity.java
@@ -2,16 +2,21 @@ package www.ctrip.com.listviewarrayadaptersimpleadapter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemClickListener,AbsListView.OnScrollListener{
     private ListView mListView;
     private ArrayAdapter<String> mArrayAdapter;
     private List<Map<String,Object>> dataList;
@@ -41,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
         mSimpleAdapter = new SimpleAdapter(this,getData(), R.layout.item,new String[]{"pic","text"},new int[]{
             R.id.pic, R.id.text});
         mListView.setAdapter(mSimpleAdapter);
-
+        mListView.setOnScrollListener(this);
+        mListView.setOnItemClickListener(this);
 
     }
 
@@ -53,5 +59,40 @@ public class MainActivity extends AppCompatActivity {
             dataList.add(map);
         }
         return dataList;
+    }
+
+    /*
+    *
+    * */
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        switch (scrollState){
+            case SCROLL_STATE_FLING:
+                Log.i("Main","用户再手指离开屏幕之前，由于用力滑了一下，视图仍在滚动");
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put("pic", R.mipmap.ic_launcher);
+                map.put("text","增加项目");
+                dataList.add(map);
+                mSimpleAdapter.notifyDataSetChanged();
+                break;
+            case SCROLL_STATE_IDLE:
+                Log.i("Main","视图已经停止滑动");
+                break;
+            case SCROLL_STATE_TOUCH_SCROLL:
+                Log.i("Mian","手指没有离开屏幕，视图正在滑动");
+                break;
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+    }
+
+    /*
+    * item点击监听器
+    * */
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String text = mListView.getItemAtPosition(position) + "";
+        Toast.makeText(this,"position=" + position + "text=" + text,Toast.LENGTH_SHORT).show();
     }
 }

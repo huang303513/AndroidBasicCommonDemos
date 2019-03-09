87db30a215b8d2fb3db6768954e9d724c86a6c49
huangchengdu <huangchengedu@innotechx.com>
2019-03-09 15:54:10 +0800

    项目升级编译

--- android布局的学习/FragmentTest/app/src/main/java/com/ctrip/www/fragmenttest/LeftFragment.java
+++ android布局的学习/FragmentTest/app/src/main/java/com/ctrip/www/fragmenttest/LeftFragment.java
@@ -3,19 +3,12 @@ package com.ctrip.www.fragmenttest;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LeftFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LeftFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LeftFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- android布局的学习/FragmentTest/app/src/main/java/com/ctrip/www/fragmenttest/MainActivity.java
+++ android布局的学习/FragmentTest/app/src/main/java/com/ctrip/www/fragmenttest/MainActivity.java
@@ -1,7 +1,8 @@
 package com.ctrip.www.fragmenttest;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         /**
          * 在活动力火气fragment对象,从而调用他的方法
          */
-        RightFragment rightFragment = (RightFragment) getFragmentManager().findFragmentById(R.id
-                .right_fragment);
+//        RightFragment rightFragment = getSupportFragmentManager().findFragmentById(R.id.right_fragment);
     }
 
     @Override
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.button:
                 AnotherRightFragment fragment = new AnotherRightFragment();
-                android.app.FragmentManager fragmentManager = getFragmentManager();
+                FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.right_layout,fragment);
                 transaction.addToBackStack(null);

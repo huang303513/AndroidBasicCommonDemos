4a2fe037a2990aaa8579197710a67ef92104d928
huangchengdu <huang2009303513@gmail.com>
2019-03-12 23:33:05 +0800

    文件存储相关操作

---
+++ 12_SharedPreferencesTest/app/src/main/java/www/huangchengdu/com/a12_sharedpreferencestest/MainActivity.java
@@ -0,0 +1,43 @@
+package www.huangchengdu.com.a12_sharedpreferencestest;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button saveData = findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name", "隔壁老黄");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", true);
+                editor.apply();
+            }
+        });
+
+        Button restoreData = findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "========>" + name);
+                Log.d("MainActivity", "========>" + age);
+                Log.d("MainActivity", "========>" + married);
+            }
+        });
+    }
+}

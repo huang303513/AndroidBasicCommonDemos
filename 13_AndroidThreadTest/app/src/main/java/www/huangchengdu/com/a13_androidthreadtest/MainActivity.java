1e17079f204861c9b894025358c21a7e2f0d2166
huangchengdu <huang2009303513@gmail.com>
2019-03-14 00:56:59 +0800

    多线程和服务相关知识点的基本使用。

---
+++ 13_AndroidThreadTest/app/src/main/java/www/huangchengdu/com/a13_androidthreadtest/MainActivity.java
@@ -0,0 +1,56 @@
+package www.huangchengdu.com.a13_androidthreadtest;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int UPDATE_TEXT = 1;
+
+    private TextView text;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        Button changeText = findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ 14_ServiceTest/app/src/main/java/www/huangchengdu/com/a14_servicetest/MyIntentService.java
@@ -0,0 +1,60 @@
+package www.huangchengdu.com.a14_servicetest;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class MyIntentService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    public static final String ACTION_FOO = "www.huangchengdu.com.a14_servicetest.action.FOO";
+    public static final String ACTION_BAZ = "www.huangchengdu.com.a14_servicetest.action.BAZ";
+
+    // TODO: Rename parameters
+    public static final String EXTRA_PARAM1 = "www.huangchengdu.com.a14_servicetest.extra.PARAM1";
+    public static final String EXTRA_PARAM2 = "www.huangchengdu.com.a14_servicetest.extra.PARAM2";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionFoo(param1, param2);
+            } else if (ACTION_BAZ.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionBaz(param1, param2);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

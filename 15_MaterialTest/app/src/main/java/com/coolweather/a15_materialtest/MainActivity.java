69d1ae614623f6fbc764078c74471728da404b01
huangchengdu <huang2009303513@gmail.com>
2019-04-13 22:14:59 +0800

    CoordinatorLayout、AppBarLayout、SwipeRefreshLayout等Material Design组件的使用。

--- 15_MaterialTest/app/src/main/java/com/coolweather/a15_materialtest/MainActivity.java
+++ 15_MaterialTest/app/src/main/java/com/coolweather/a15_materialtest/MainActivity.java
@@ -6,18 +6,43 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+
+    private SwipeRefreshLayout swipeRefresh;
+
+    private Fruit[] fruits = {
+            new Fruit("Apple", R.drawable.apple),
+            new Fruit("Banana", R.drawable.banana),
+            new Fruit("Orange", R.drawable.orange),
+            new Fruit("Watermelon", R.drawable.watermelon),
+            new Fruit("Pear", R.drawable.pear),
+            new Fruit("Grape", R.drawable.grape),
+            new Fruit("Pineapple", R.drawable.pineapple),
+            new Fruit("Strawberry", R.drawable.strawberry),
+            new Fruit("Cherry", R.drawable.cherry),
+            new Fruit("Mango", R.drawable.mango)};
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +82,24 @@ public class MainActivity extends AppCompatActivity {
                 }).show();
             }
         });
+
+
+        swipeRefresh = findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+
+
+        initFruits();
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -83,5 +126,35 @@ public class MainActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    private void refreshFruits(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void initFruits(){
+        fruitList.clear();
+        for (int i = 0; i < 50; i++){
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
 }

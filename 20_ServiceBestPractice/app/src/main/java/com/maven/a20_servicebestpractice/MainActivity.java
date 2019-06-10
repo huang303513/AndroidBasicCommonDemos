75cba04526505201c31041fd6aede1eb850774fc
huangchengdu <huang2009303513@gmail.com>
2019-06-10 17:38:07 +0800

    update

---
+++ 20_ServiceBestPractice/app/src/main/java/com/maven/a20_servicebestpractice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.maven.a20_servicebestpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
